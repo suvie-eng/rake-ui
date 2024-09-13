f6b459d6f5aed7e0b51c15b320fd36f888aa5e7c
muzsuvie <mmujahid@suvie.com>
2024-09-14 00:14:10 +0500

    udpate migration setup

--- lib/rake-ui/engine.rb
+++ lib/rake-ui/engine.rb
@@ -7,5 +7,14 @@ require "open3"
 module RakeUi
   class Engine < ::Rails::Engine
     isolate_namespace RakeUi
+    initializer "rake_ui.load_migrations" do |app|
+      if RakeUi.configuration.active_storage
+        unless app.root.to_s.match?(RakeUi::Engine.root.to_s)
+          app.config.paths['db/migrate'].concat(
+            RakeUi::Engine.paths['db/migrate'].existent
+          )
+        end
+      end
+    end
   end
 end
